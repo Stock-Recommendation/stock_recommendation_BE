8538316d653aef66d6691cb3b4df85ad48984794
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-15 21:44:27 +0700

    first rest api

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,6 +2,6 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/"
-endpoint = "http://localhost:8000/" #http://127.0.0.1:8000/
+endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/
 get_response = requests.get(endpoint)
 print(get_response.text)
