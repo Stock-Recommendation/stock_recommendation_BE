1af4f1695ef2389b099852bc36ee1e55fa9c30a4
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-15 12:18:57 +0700

    finish setup

---
+++ py_client/basic.py
@@ -0,0 +1,7 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/"
+endpoint = "http://localhost:8000/" #http://127.0.0.1:8000/
+get_response = requests.get(endpoint)
+print(get_response.text)
