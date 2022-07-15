5161f6f36a5c30864375ac90c605f9d328a990ea
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-15 13:03:19 +0700

    first api

--- backend/rest/urls.py
+++ backend/rest/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+# from api.views import api_home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api/', api_home, 'api home') #localhost/api
+    path('api/', include('api.urls')) #better pagination
 ]
+
