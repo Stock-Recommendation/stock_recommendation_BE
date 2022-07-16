ab95e07b6d19e5ce8c4cf1f83f5daa5a60feb383
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-16 22:28:52 +0700

    api response as model instance

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,15 @@
+from statistics import mode
 from django.http import JsonResponse
-
+import json
+from products.models import Product
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "hey there"})
+    model_data = Product.objects.all().order_by("?").first()
+    # print(model_data)
+    data= {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse(data, safe=False)
+
