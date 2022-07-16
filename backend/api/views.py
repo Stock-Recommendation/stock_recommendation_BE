d94451ae80922218440f77abf321f28c6fbd67b1
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-16 22:45:25 +0700

    http response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,17 @@
 from statistics import mode
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpRequest
 import json
+from django.forms.models import model_to_dict
 from products.models import Product
-def api_home(request, *args, **kwargs):
+def api_home(request: HttpRequest, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     # print(model_data)
     data= {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data, safe=False)
+        data = model_to_dict(model_data, fields=['id', 'price'])
+        json_data_str = json.dumps(data)
+        print(data)
+    # return HttpResponse(data), return type html/text
+    # return HttpResponse(json_data_str, headers={'content-type': "application/json"}) 
+    return JsonResponse(data)
 
