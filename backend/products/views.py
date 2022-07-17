294afa320f0d09515b8e88190de5fb4eabdbabe5
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-07-17 18:17:51 +0700

    use alt view

--- backend/products/views.py
+++ backend/products/views.py
@@ -2,8 +2,9 @@ from rest_framework import generics
 
 from .models import Product
 from .serializers import ProductSerializer
-
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -22,5 +23,23 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 product_detail_view = ProductDetailAPIView.as_view()
 
-
+@api_view(['GET', 'POST'])
+def product_alt_view(request,pk=None, *args, **kwargs):
+    method = request.method
     
+    if method =='GET':
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk) 
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        qs = Product.objects.all()
+        data = ProductSerializer(qs, many=True).data
+        return Response(data)
+    if method == 'POST':
+        serializer = ProductSerializer(data = request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid":"bad request"}, status=404)
