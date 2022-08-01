b00ae5aae008a640a1ce2478e4c3e053af34688b
kien-to <ktto@umass.edu>
2022-08-01 17:59:31 +0700

    cron and model change

--- py_client/stock.py
+++ py_client/stock.py
@@ -1,28 +1,30 @@
-# import finnhub  # https://finnhub.io/docs/api/introduction
-# import pandas as pd
-# import calendar
-# import datetime
-# from datetime import datetime, timedelta, timezone
-# # from stocks import Stock
+import finnhub  # https://finnhub.io/docs/api/introduction
+import pandas as pd
+import calendar
+import datetime
+from datetime import datetime, timedelta, timezone
+# from stocks import Stock
 
-# todayUTC = datetime.now(timezone.utc)
-# lastYearUTC = todayUTC - timedelta(365)
+todayUTC = datetime.now(timezone.utc)
+lastYearUTC = todayUTC - timedelta(365)
 
-# utc_time_to = calendar.timegm(todayUTC.utctimetuple())
-# utc_time_from = calendar.timegm(lastYearUTC.utctimetuple())
+utc_time_to = calendar.timegm(todayUTC.utctimetuple())
+utc_time_from = calendar.timegm(lastYearUTC.utctimetuple())
 
-# finnhub_client = finnhub.Client(api_key="cbdmpuaad3i64n13s900")
-# # print(res)
-# # print(pd.DataFrame(res))
-
-# stocks = finnhub_client.indices_const(symbol="^OEX")['constituents'][:15]
-# res = []
-# for stock in stocks:
-#     ticker = stock
-#     name = finnhub_client.company_profile2(symbol=stock)["name"]
-#     market_cap = finnhub_client.company_profile2(symbol=stock)["marketCapitalization"]
-#     # web_url = finnhub_client.company_profile2(symbol=stock)["weburl"]
-#     current_price = finnhub_client.quote(stock)["c"]
-#     # print(stock)
-#     res.append([ticker, name, market_cap, current_price])
+finnhub_client = finnhub.Client(api_key="cbdmpuaad3i64n13s900")
 # print(res)
+# print(pd.DataFrame(res))
+
+stocks = finnhub_client.indices_const(symbol="^OEX")['constituents'][:15]
+res = []
+for stock in stocks:
+    ticker = stock
+    name = finnhub_client.company_profile2(symbol=stock)["name"]
+    market_cap = finnhub_client.company_profile2(symbol=stock)["marketCapitalization"]
+    # web_url = finnhub_client.company_profile2(symbol=stock)["weburl"]
+    # historical_price = finnhub_client.stock_candles(stock, 'M', utc_time_from, utc_time_to)['c']
+    # time_stamp = finnhub_client.stock_candles(stock, 'M', utc_time_from, utc_time_to)['t']
+    current_price = finnhub_client.quote(stock)["c"]
+    # print(stock)
+    res.append([ticker, name, market_cap, current_price])
+print(res)
