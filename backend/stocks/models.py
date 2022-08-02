5763c05cf540b38e699abb525b6acf1a1cc0525f
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2022-08-02 16:00:05 +0700

    sset up

--- backend/stocks/models.py
+++ backend/stocks/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 class Stock(models.Model):
     ticker = models.CharField(max_length=4)
-    # stock_id = models.IntegerField()
     stock_name = models.CharField(max_length=200)
     market_cap = models.IntegerField()
     current_price = models.FloatField(0.0)
-    # time_stamp = models.DateTimeField() -> call on the front-ends
-    # historical_price = models.IntegerField() -> call on the front-ends
     predicted_price_1d = models.FloatField()
     predicted_price_1w = models.FloatField()
     predicted_price_1m = models.FloatField()
     predicted_price_3m = models.FloatField()
+    # historical_price = models.IntegerField() -> call on the front-ends
+    # time_stamp = models.DateTimeField() -> call on the front-ends
     # tweet_ids = models.CharField() -> call on the front-ends
     # historical_accuracy = models.FloatField()
+    # stock_id = models.IntegerField()
 
 # update()
--- backend/stocks/prepare_data/create.py
+++ backend/stocks/prepare_data/create.py
@@ -5,17 +5,17 @@ import datetime
 from datetime import datetime, timedelta, timezone
 from stocks.models import Stock
 import cronjobs
-import pandas as pd
 
 todayUTC = datetime.now(timezone.utc)
 lastYearUTC = todayUTC - timedelta(365)
 utc_time_to = calendar.timegm(todayUTC.utctimetuple())
 utc_time_from = calendar.timegm(lastYearUTC.utctimetuple())
 finnhub_client = finnhub.Client(api_key="cbdmpuaad3i64n13s900")
-
 # ML model with cronjobs scheduled
 # consider putting @cronjobs.register here
+
 def process(stock) -> pd.DataFrame:
+    tweet_ids = []
     historical_price = finnhub_client.stock_candles(
         stock, 'M', utc_time_from, utc_time_to)['c']
     time_stamp = finnhub_client.stock_candles(
@@ -26,7 +26,7 @@ def process(stock) -> pd.DataFrame:
 
 # main function to process everything with cronjobs scheduled
 @cronjobs.register
-def create() -> list:
+def create():
     stocks = finnhub_client.indices_const(symbol="^OEX")['constituents'][:30]
 
     for stock in stocks:
@@ -39,11 +39,10 @@ def create() -> list:
             1], process(stock)[2], process(stock)[3]
 
         # get list of tweet ids for this stock here
-        tweet_ids = []
 
         st = Stock(ticker=ticker, stock_name=stock_name,
                    market_cap=market_cap, current_price=current_price,
-                   tweet_ids=tweet_ids, predicted_price_1d=predicted_price_1d, predicted_price_1w=predicted_price_1w, predicted_price_1m=predicted_price_1m, predicted_price_3m=predicted_price_3m)
+                   predicted_price_1d=predicted_price_1d, predicted_price_1w=predicted_price_1w, predicted_price_1m=predicted_price_1m, predicted_price_3m=predicted_price_3m)
 
         # if stock already exists, update
         if (len(Stock.objects.filter(ticker=stock)) != 0):
